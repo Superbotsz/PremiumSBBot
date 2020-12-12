a0a58ae9c72f1c890398ce3281da0126fd9664cd
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-12 19:14:55 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,8 @@
 ###############################################
 
-Follow me on Youtube : https://www.youtube.com/c/cvatricks
 
-Live Bot Link : https://t.me/PremiumSBBot
+Follow_me_on_Youtube = "https://www.youtube.com/c/cvatricks"
+
+Live_Bot_Link = "https://t.me/PremiumSBBot"
 
 ################################################
