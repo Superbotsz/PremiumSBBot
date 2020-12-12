21d48f08138489d1ad1eb3c39125d9d5b4c8af1b
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-12 18:46:00 +0530

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,7 @@
+###############################################
+
+Follow me on Youtube : https://www.youtube.com/c/cvatricks
+
+Live Bot Link : https://t.me/PremiumSBBot
+
+################################################
